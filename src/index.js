d3ead343fe402450fc9e50fdb0d730cdc0ed0a41
Evan Turner <evturn@gmail.com>
2017-10-01 17:08:44 -0400

    Add additional arguments to connect

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-const connectDecorator = (matchPropsToState, actions) => {
+const connectDecorator = (matchPropsToState, actions, ...args) => {
   const provideDispatch = dispatch => bindActionCreators(actions, dispatch);
   const matchDispatchToProps = !actions ? actions : provideDispatch;
-  return connect(matchPropsToState, matchDispatchToProps);
+  return connect(matchPropsToState, matchDispatchToProps, ...args);
 };
 
 export default connectDecorator;
